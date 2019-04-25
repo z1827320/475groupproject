5703a523f6cb2192411fa96e5be5453fde24986e
Dillon Larson <46943554+z1827320@users.noreply.github.com>
2019-04-25 16:28:13 -0500

    morethings

--- info.js
+++ info.js
@@ -1,15 +1,15 @@
 function my352() {
-    document.getElementById("demo").innerHTML = "Focuses on the application and integration of concepts, processes, and tools to plan and manage business projects. Topics will include the core areas of project management as defined in the Project Management Body of Knowledge (PMBOK).";
+    document.getElementById("demo").innerHTML = "<h4>Managing Projects in Business</h4> <p>Focuses on the application and integration of concepts, processes, and tools to plan and manage business projects. Topics will include the core areas of project management as defined in the Project Management Body of Knowledge (PMBOK).</p> <h4>Prerequisites & Notes</h4> <p>PRQ: OMIS 338 and OMIS 351.</p>";
 }
 function my449() {
-    document.getElementById("demo").innerHTML = "Study of programming logic for designing and developing business-based applications. Extensive experience with a contemporary programming language in common use in business.";
+    document.getElementById("demo").innerHTML = "<h4>Business Application Developent</h4> <p>Study of programming logic for designing and developing business-based applications. Extensive experience with a contemporary programming language in common use in business.</p> <h4>Prerequisites & Notes</h4> <p>PRQ: OMIS 338 and OMIS 351.</p>";
 }
 function my460() {
-    document.getElementById("demo").innerHTML = "Survey of various methodologies and techniques utilized in interfacing telecommunication systems within the business environment, along with the consideration of issues related to the management of these telecommunications systems.";
+    document.getElementById("demo").innerHTML = "<h4>Business Computing Environment & Networks</h4> <p>Survey of various methodologies and techniques utilized in interfacing telecommunication systems within the business environment, along with the consideration of issues related to the management of these telecommunications systems.</p> <h4>Prerequisites & Notes</h4> <p>PRQ: OMIS 338 and OMIS 351.</p>";
 }
 function my462() {
-    document.getElementById("demo").innerHTML = "In-depth study of the concepts, methods, and tools used for business system development and project management. Topics include project design and estimating, the system development life cycle, change management, and quality assurance.";
+    document.getElementById("demo").innerHTML = "<h4>Business Systems Analysis & Design</h4> <p>In-depth study of the concepts, methods, and tools used for business system development and project management. Topics include project design and estimating, the system development life cycle, change management, and quality assurance.</p> <h4>Prerequisites & Notes</h4> <p>PRQ: OMIS 452.</p>";
 }
 function my475() {
-    document.getElementById("demo").innerHTML = "Overview of electronic business technology including use of information technology tools to design and develop business-to-business applications. Impact of information technology design on electronic business strategy, planning, implementation, and operation.";
+    document.getElementById("demo").innerHTML = "<h4>Internet & Web Computing Technologies</h4> <p>Overview of electronic business technology including use of information technology tools to design and develop business-to-business applications. Impact of information technology design on electronic business strategy, planning, implementation, and operation.</p> <h4>Prerequisites & Notes</h4> <p>PRQ: OMIS 452. CRQ: OMIS 449 or consent of department.</p>";
 }
