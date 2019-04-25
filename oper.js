a8b95b2f435f01febe73f32d941580c312a2941c
Dillon Larson <46943554+z1827320@users.noreply.github.com>
2019-04-25 15:44:53 -0500

    cha

---
+++ oper.js
@@ -0,0 +1,15 @@
+function my324() {
+    document.getElementById("demo").innerHTML = "Crosslisted as STAT 324X. Introduction to business analytic tools focusing on sampling, multivariate regression, factor analysis and cluster analysis to identify and solve business problems";
+}
+function my472() {
+    document.getElementById("demo").innerHTML = "Introduction to concepts in business intelligence (BI) and exploration of how BI can improve management effectiveness through better decision-making. Exploration of the development of BI through the use of dashboards, management reporting, and performance measurement. Hands-on experience using current BI tools in the areas of data warehousing, data marts, business analytics, web analytics, real-time data, and mobile applications";
+}
+function my473() {
+    document.getElementById("demo").innerHTML = "Introduction to basic data visualization design and evaluation principles, including techniques for visualizing multivariate, text-based, temporal, geospatial, hierarchical, and network/graph-based data in the business world.";
+}
+function my474() {
+    document.getElementById("demo").innerHTML = "Overview of key concepts, tools, and analysis techniques of quantitative internet data to optimize websites and web marketing initiatives, including search engine optimization, tracking, and website traffic via pay per click.";
+}
+function my482() {
+    document.getElementById("demo").innerHTML = "Study of advanced topics in predictive business analytics. Topics include data visualization, time series analysis, forecasting and advanced data mining. Emphasis on using predictive business analytics software such as R, SAS, and SAP Predictive Analytics on case studies and projects to see the important real-world implications of predictive analytics in business.";
+}
