2a4996b5baf379fa77f1e4eed5ff164b125c7e7a
Dillon Larson <46943554+z1827320@users.noreply.github.com>
2019-04-25 16:05:44 -0500

    c

--- info.js
+++ info.js
@@ -1,15 +1,15 @@
-function my324() {
-    document.getElementById("demo").innerHTML = "Crosslisted as STAT 324X. Introduction to business analytic tools focusing on sampling, multivariate regression, factor analysis and cluster analysis to identify and solve business problems";
+function my352() {
+    document.getElementById("demo").innerHTML = "Focuses on the application and integration of concepts, processes, and tools to plan and manage business projects. Topics will include the core areas of project management as defined in the Project Management Body of Knowledge (PMBOK).";
 }
-function my472() {
-    document.getElementById("demo").innerHTML = "Introduction to concepts in business intelligence (BI) and exploration of how BI can improve management effectiveness through better decision-making. Exploration of the development of BI through the use of dashboards, management reporting, and performance measurement. Hands-on experience using current BI tools in the areas of data warehousing, data marts, business analytics, web analytics, real-time data, and mobile applications";
+function my449() {
+    document.getElementById("demo").innerHTML = "Study of programming logic for designing and developing business-based applications. Extensive experience with a contemporary programming language in common use in business.";
 }
-function my473() {
-    document.getElementById("demo").innerHTML = "Introduction to basic data visualization design and evaluation principles, including techniques for visualizing multivariate, text-based, temporal, geospatial, hierarchical, and network/graph-based data in the business world.";
+function my460() {
+    document.getElementById("demo").innerHTML = "Survey of various methodologies and techniques utilized in interfacing telecommunication systems within the business environment, along with the consideration of issues related to the management of these telecommunications systems.";
 }
-function my474() {
-    document.getElementById("demo").innerHTML = "Overview of key concepts, tools, and analysis techniques of quantitative internet data to optimize websites and web marketing initiatives, including search engine optimization, tracking, and website traffic via pay per click.";
+function my462() {
+    document.getElementById("demo").innerHTML = "In-depth study of the concepts, methods, and tools used for business system development and project management. Topics include project design and estimating, the system development life cycle, change management, and quality assurance.";
 }
-function my482() {
-    document.getElementById("demo").innerHTML = "Study of advanced topics in predictive business analytics. Topics include data visualization, time series analysis, forecasting and advanced data mining. Emphasis on using predictive business analytics software such as R, SAS, and SAP Predictive Analytics on case studies and projects to see the important real-world implications of predictive analytics in business.";
+function my475() {
+    document.getElementById("demo").innerHTML = "Overview of electronic business technology including use of information technology tools to design and develop business-to-business applications. Impact of information technology design on electronic business strategy, planning, implementation, and operation.";
 }
