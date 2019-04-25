636cc1668834534cc889fb34245c58601b7226e3
Dillon Larson <46943554+z1827320@users.noreply.github.com>
2019-04-25 16:47:48 -0500

    change

---
+++ core.js
@@ -0,0 +1,12 @@
+function my340() {
+    document.getElementById("demo").innerHTML = "<h4>Supply Chain Management</h4> <p>Comprehensive study of concepts, processes, and strategies used in the development and management of global supply chains. Topics include procurement, electronic commerce, information technologies, global supply chain management, and logistics activities.</p> <h4>Prerequisites & Notes</h4> <p>PRQ: OMIS 338 and OMIS 351. CRQ: FINA 320 and MGMT 335 and MKTG 295.</p>";
+}
+function my442() {
+document.getElementById("demo").innerHTML = "<h4>Process & Quality Improvement</h4> <p>Detailed examination of the issues, techniques,and methodologies for improving processes and controlling quality in manufacturing and service organizations. Topics include the history of process and quality improvement, quality culture, customer-driven quality, six sigma methods, tools for process improvement, statistical process control, and building and sustaining performance excellence.</p> <h4>Prerequisites & Notes</h4> <p>PRQ: OMIS 338 and OMIS 351. CRQ: FINA 320 and MGMT 335 and MKTG 295.</p>";
+}
+function my452() {
+    document.getElementById("demo").innerHTML = "<h4>Database Management</h4> <p>Comparison of database management packages for business computers with laboratory exercises that deal with the development of business applications. Practical experience with the most current database management packages.</p> <h4>Prerequisites & Notes</h4> <p>PRQ: OMIS 351. CRQ: OMIS 338 and FINA 320 and MGMT 335 and MKTG 295.</p>";
+}
+function my455() {
+    document.getElementById("demo").innerHTML = "<h4>Enterprise Resource Planning</h4> <p>In-depth analysis of current business enterprise systems; their system components, database, information requirements, and system interfaces. Commercial software used to illustrate the concepts presented.</p> <h4>Prerequisites & Notes</h4> <p>PRQ: OMIS 338 and OMIS 351. CRQ: FINA 320 and MGMT 335 and MKTG 295.</p>";
+}
