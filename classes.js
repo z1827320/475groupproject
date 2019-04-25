e3e13f3db01014ddb28fc02f9f15d9e4be1ce8e3
Dillon Larson <46943554+z1827320@users.noreply.github.com>
2019-04-25 15:15:31 -0500

    classes changes

---
+++ classes.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Crosslisted as STAT 324X. Introduction to business analytic tools focusing on sampling, multivariate regression, factor analysis and cluster analysis to identify and solve business problems";
+}
