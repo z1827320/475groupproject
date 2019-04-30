c946a93f0080b69a8ea8ab0c9f6149e15066dec7
Dillon Larson <46943554+z1827320@users.noreply.github.com>
2019-04-30 14:19:47 -0500

    changes

--- data.js
+++ data.js
@@ -1,15 +1,15 @@
 function my324() {
-    document.getElementById("demo").innerHTML = "Crosslisted as STAT 324X. Introduction to business analytic tools focusing on sampling, multivariate regression, factor analysis and cluster analysis to identify and solve business problems";
+    document.getElementById("demo").innerHTML = "<h4>Intrduction to Business Data Analytics</h4> <p>Crosslisted as STAT 324X. Introduction to business analytic tools focusing on sampling, multivariate regression, factor analysis and cluster analysis to identify and solve business problems</p> <h4>Prerquisites & Notes</h4> <p>PRQ: UBUS 223 or STAT 301</p>";
 }
 function my472() {
-    document.getElementById("demo").innerHTML = "Introduction to concepts in business intelligence (BI) and exploration of how BI can improve management effectiveness through better decision-making. Exploration of the development of BI through the use of dashboards, management reporting, and performance measurement. Hands-on experience using current BI tools in the areas of data warehousing, data marts, business analytics, web analytics, real-time data, and mobile applications";
+    document.getElementById("demo").innerHTML = "<h4>Intrduction to Business Intelligence</h4> <p>Introduction to concepts in business intelligence (BI) and exploration of how BI can improve management effectiveness through better decision-making. Exploration of the development of BI through the use of dashboards, management reporting, and performance measurement. Hands-on experience using current BI tools in the areas of data warehousing, data marts, business analytics, web analytics, real-time data, and mobile applications</p> <h4>Prerequisites & Notes</h4> <p>PRQ: OMIS 452 CRQ: OMIS 324</p>";
 }
 function my473() {
-    document.getElementById("demo").innerHTML = "Introduction to basic data visualization design and evaluation principles, including techniques for visualizing multivariate, text-based, temporal, geospatial, hierarchical, and network/graph-based data in the business world.";
+    document.getElementById("demo").innerHTML = "<h4>Data Visualization for Business</h4> <p>Introduction to basic data visualization design and evaluation principles, including techniques for visualizing multivariate, text-based, temporal, geospatial, hierarchical, and network/graph-based data in the business world.</p> <h4>Prerequisites & Notes</h4> <p>PRQ: OMIS 452 CRQ: 324</p>";
 }
 function my474() {
-    document.getElementById("demo").innerHTML = "Overview of key concepts, tools, and analysis techniques of quantitative internet data to optimize websites and web marketing initiatives, including search engine optimization, tracking, and website traffic via pay per click.";
+    document.getElementById("demo").innerHTML = " <h4>Digital Analytics</h4> <p>Overview of key concepts, tools, and analysis techniques of quantitative internet data to optimize websites and web marketing initiatives, including search engine optimization, tracking, and website traffic via pay per click.</p> <h4>Prerequisites & Notes</h4> <p>PRQ: OMIS 452 CRQ: OMIS 324</p>";
 }
 function my482() {
-    document.getElementById("demo").innerHTML = "Study of advanced topics in predictive business analytics. Topics include data visualization, time series analysis, forecasting and advanced data mining. Emphasis on using predictive business analytics software such as R, SAS, and SAP Predictive Analytics on case studies and projects to see the important real-world implications of predictive analytics in business.";
+    document.getElementById("demo").innerHTML = "<h4>Predictive Business Analytics</h4> <p>Study of advanced topics in predictive business analytics. Topics include data visualization, time series analysis, forecasting and advanced data mining. Emphasis on using predictive business analytics software such as R, SAS, and SAP Predictive Analytics on case studies and projects to see the important real-world implications of predictive analytics in business.<p> <h4>Prerequisites & Notes</h4> <p>PRQ: OMIS 324 and OMIS 452</p>";
 }
--- oper.js
+++ oper.js
@@ -1,15 +1,15 @@
 function my327() {
-    document.getElementById("demo").innerHTML = "Crosslisted as STAT 324X. Introduction to business analytic tools focusing on sampling, multivariate regression, factor analysis and cluster analysis to identify and solve business problems";
+    document.getElementById("demo").innerHTML = "<h4>Decision Analysis</h4> <p>Study of the tools, models, and quantitative techniques used in the operations management discipline. Topics include forecasting, inventory models, queuing theory, statistical models, decision theory, mathematical programming, simulation, and project management.</p> <h4>Prerequisites & Notes</h4> <p>CRQ: OMIS 338</p>";
 }
 function my444() {
-    document.getElementById("demo").innerHTML = "Introduction to concepts in business intelligence (BI) and exploration of how BI can improve management effectiveness through better decision-making. Exploration of the development of BI through the use of dashboards, management reporting, and performance measurement. Hands-on experience using current BI tools in the areas of data warehousing, data marts, business analytics, web analytics, real-time data, and mobile applications";
+    document.getElementById("demo").innerHTML = "<h4>Manufacturing Systems Management</h4> <p>Detailed investigation of the quantitative issues related to the planning, control, and design of manufacturing and supply chain systems. Topics include materials management, capacity planning, and resource and product scheduling.</p> <h4>Prerequisites & Notes</h5> <p>PRQ: OMIS 338 and OMIS 351</p>";
 }
 function my450() {
-    document.getElementById("demo").innerHTML = "Introduction to basic data visualization design and evaluation principles, including techniques for visualizing multivariate, text-based, temporal, geospatial, hierarchical, and network/graph-based data in the business world.";
+    document.getElementById("demo").innerHTML = "<h4>Service Operations</h4> <p>Analysis of the application of operations management concepts to service organizations. Strategic and tactical issues of managing service organizations such as: airline, financial, retail, health care, hotel/resort, and restaurant service delivery systems, will be examined. Topics include service strategy, globalization of services, quality, location selection, layout design, and capacity management as applied to service environments</p> <h4>Prerequisies & Notes</h4> <p>PRQ: OMIS 338 and OMIS 351</p>";
 }
 function my478() {
-    document.getElementById("demo").innerHTML = "Overview of key concepts, tools, and analysis techniques of quantitative internet data to optimize websites and web marketing initiatives, including search engine optimization, tracking, and website traffic via pay per click.";
+    document.getElementById("demo").innerHTML = "<h4>Predictive Business Analytics</h4> <p>Study of advanced topics in predictive business analytics. Topics include data visualization, time series analysis, forecasting and advanced data mining. Emphasis on using predictive business analytics software such as R, SAS, and SAP Predictive Analytics on case studies and projects to see the important real-world implications of predictive analytics in business.</p> <h4>Prerequisites & Notes</h4> <p>PRQ: OMIS 324 and OMIS 452</p>";
 }
 function my480() {
-    document.getElementById("demo").innerHTML = "Study of advanced topics in predictive business analytics. Topics include data visualization, time series analysis, forecasting and advanced data mining. Emphasis on using predictive business analytics software such as R, SAS, and SAP Predictive Analytics on case studies and projects to see the important real-world implications of predictive analytics in business.";
+    document.getElementById("demo").innerHTML = "<h4>Advanced Supply Chain Management</h4> <p>Study of current and future developments, trends, and technologies in supply chain management. Contemporary readings, presentations, and projects used to define modern supply chain management practice and thought.</p> <h4>Prerequisites & Notes</h4> <p>PRQ: OMIS 340 and OMIS 452. CRQ: OMIS 327</p>";
 }
